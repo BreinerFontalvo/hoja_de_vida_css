3b16a20bcffabd665e7578d5f8686f005df654dd
BreinerFontalvo <fontalvob71@gmail.com>
2022-11-02 09:02:49 -0500

    listo mi hoja de vida

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# CV-CSS
+Hoja de vida con HTML Y CSS
+=======
+# hoja_de_vida_css
+>>>>>>> d56e882b737d7fbbc8f431595451c99d6a1e2de2
---
+++ app.js/main.js
@@ -0,0 +1,8 @@
+
+var nombre ="breyner fontalvo"
+var altura =190;
+
+var concatenacion=nombre +" " +altura;
+
+var datos = document.getElementById("datos");
+datos.innerHTML =concatenacion;
